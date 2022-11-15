b1c600c054d9380b8a0453104dd5a8a92b7e0b07
Kasa-Vardhan <kasa.saiharshavardhan@gmail.com>
2022-11-15 20:52:39 +0530

    changes commited

---
+++ script.js
@@ -0,0 +1,26 @@
+//                                                                                  ARRAY METHODS
+
+// let arr = ["apple", "mango"];
+
+// arr.forEach((item, index, arr)=>{
+//     console.log(`${item} is at ${index} in ${arr}`)
+// });
+
+// let users = [
+//     {id: 1, empNam: "John"},
+//     {id: 2, empNam: "Peter"},
+// ];
+
+// let user = users.find(item => item.id < 3);
+// console.log(user);
+
+// let users = [
+//   { id: 1, name: "John" },
+//   { id: 2, name: "Pete" },
+//   { id: 3, name: "Mary" },
+// ];
+// let someUsers = users.filter((item) => item.id == 3);
+// console.log(someUsers);
+
+// let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
+// console.log(lengths);
